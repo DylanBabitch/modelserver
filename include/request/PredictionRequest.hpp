752ae1f0e45d98ec413142a91480f7a3a094c64f
Dylan Babitch <DylanBab745@gmail.com>
2026-08-14 11:55:16 -0400

    working on implementing onnx support for one model

--- include/request/PredictionRequest.hpp
+++ include/request/PredictionRequest.hpp
@@ -1,9 +1,17 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <cstdint>
 
 struct PredictionRequest {
+    struct TensorInput {
+        std::string name;
+        std::vector<std::int64_t> shape;
+        std::vector<float> data;
+    };
+
     std::string model;
     std::string version;
-    std::string input;
+    TensorInput input;
 };
